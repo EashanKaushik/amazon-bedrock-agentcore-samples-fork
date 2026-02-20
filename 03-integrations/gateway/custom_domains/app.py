797a2daeef08b8b54ed33956a8d785ce15557aa4
Eashan Kaushik <50113394+EashanKaushik@users.noreply.github.com>
2026-02-20 16:07:27 -0500

    liscence

--- 03-integrations/gateway/custom_domains/app.py
+++ 03-integrations/gateway/custom_domains/app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 import os
 
 import aws_cdk as cdk
--- 03-integrations/gateway/custom_domains/custom_domains/custom_domains_stack.py
+++ 03-integrations/gateway/custom_domains/custom_domains/custom_domains_stack.py
@@ -1,3 +1,4 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 import aws_cdk as cdk
 from aws_cdk import (
     CfnOutput,
